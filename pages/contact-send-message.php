619b38b09d5f657fbb11a1b27754b1e98b8ae0b4
tog22 <tom.ash.github@philosofiles.com>
2021-01-26 21:09:54 -0800

    Sending contact messages

---
+++ pages/contact-send-message.php
@@ -0,0 +1,11 @@
+<?php
+	
+/*
+require '../libraries/php_mailer/src/PHPMailer.php';
+require '../libraries/php_mailer/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+*/
