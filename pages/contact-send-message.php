afad4390439b38173f27f83da2253a584dcee88f
tog22 <tom.ash.github@philosofiles.com>
2021-01-27 12:40:19 -0800

    Contact form

--- pages/contact-send-message.php
+++ pages/contact-send-message.php
@@ -1,6 +1,7 @@
 <?php
 	
 /*
+	start of phpmailer v:
 require '../libraries/php_mailer/src/PHPMailer.php';
 require '../libraries/php_mailer/src/Exception.php';
 
@@ -9,3 +10,24 @@ use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer(true);
 */
+
+	
+ob_start();
+	
+mail('DetertJ@darden.virginia.edu',$_POST['name'].' [via JD.com]',"(Company/org: ".$_POST['org'].")\n\n".$_POST['message']);
+
+// header("Location: http://example.com/myOtherPage.php");
+
+?>
+
+<p>Thank you for your message.</p>
+
+<p>
+	<a href="http://jimdetert.com/">
+		To continue browsing my website, click here.
+	</a>
+</p>
+
+<?
+exit();
+// die or exit, because http://thedailywtf.com/Articles/WellIntentioned-Destruction.aspx
