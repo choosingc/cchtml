6b22e5641d2e97a55cd5de80004f710a2982dd87
tog22 <tom.ash.github@philosofiles.com>
2021-02-22 15:02:11 -0800

    Contact form

--- pages/contact-send-message.php
+++ pages/contact-send-message.php
@@ -15,8 +15,11 @@ $mail = new PHPMailer(true);
 ob_start();
 	
 $subject = $_POST['name'].' [via JD.com]';
+$message_with_rest_of_form = "Company/organization:\n".$_POST['org']."\n\nEmail address:\n".'<a href="mailto:'.$_POST['email'].'">'.$_POST['email']."</a>\n\nMessage:\n\n".$_POST['message'];
+
+
 // should be jim.detert.choosingcourage@gmail.com
-mail('jim.detert.choosingcourage@gmail.com',$subject,"(Company/org: ".$_POST['org'].")\n\n".$_POST['message']);
+mail('tomashoxford@gmail.com',$subject,$message_with_rest_of_form);
 
 // header("Location: http://example.com/myOtherPage.php");
 
